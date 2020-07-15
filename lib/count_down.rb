c82ef2c09a685e0b120155c7abf687ecd8f22639
Jake <llira09@gmail.com>
2020-07-15 21:25:47 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,11 +3,11 @@
 #   puts "say this forever..."
 # end
 
-magic_exit_number = 7
-count = 0 
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!"
+# magic_exit_number = 7
+# count = 0 
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!"
   
-  count += 1
-end
+#   count += 1
+# end
